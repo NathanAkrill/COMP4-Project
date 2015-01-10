b37a9f74f51cafd5962baffccf7bce0633b5e5a1
Nathan Akrill <nathan.akrill@hotmail.co.uk>
2015-01-10 12:28:33 +0000

    Added proper sql parts (still need the ConnectorJ.jar)

--- PhysicsQuiz.java
+++ PhysicsQuiz.java
@@ -1,5 +1,20 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
+
+//Will not work until ConnectorJ Driver is in classpath
+public Connection openConnection(){
+	Properties properties = new Properties();
+	properties.put("user", "root");
+	properties.put("password", "usbw");
+	properties.put("characterEncoding, "ISO-8859-1");
+	properties.put("useUnicode, "true");
+	String url = "jdbc:mysql://localhost/pastpapers";
+	
+	Class.forName("com.mysql.jdbc.Driver").newInstance();
+	Connection c = DriverManager.getConnection(url, properties);
+	return c;
+}
 
 public class PhysicsQuiz extends Frame implements WindowListener, ActionListener{
 	private Label menuheader;
@@ -46,18 +61,32 @@ public class PhysicsQuiz extends Frame implements WindowListener, ActionListener
 		question[] questions = new question[10];
 
 		//for(int j;j<questions.length;j++){
+		//	Connection c = createConnection();
+		//	Statement st = c.createStatement();
 		//	int random = Rand.nextInt
-		//	questions[j].content = (
-		//	SELECT Content
-		//	FROM question
-		//	WHERE Topic = topic AND QuestionID = random
-		//	)
-		//	
-		//	questions[j].type = (
-		//	SELECT Type
-		//	FROM question
-		//	WHERE QuestionID = random
-		//	)
+		//	String sqlcontent = "SELECT Content FROM question WHERE Topic = " + topic "AND QuestionID = " + random;
+		//	ResultSet rs1 = st.executeQuery(sqlcontent);
+		//	ResultSet rs1 = st.getResultSet();
+		//	try{
+		//		while (rs1.next()){
+		//			questions[j].content = rs1.getString(1);		
+		//		}
+		//	} finally {
+		//		rs1.close
+		//	}
+		//	String sqltype = "SELECT Type FROM question WHERE QuestionID = " + random;
+		//	ResultSet rs2 = st.executeQuery(sqltype);
+		//	ResultSet rs2 = st.getResultSet();
+		//	try{
+		//		while (rs2.next()){
+		//			questions[j].type = rs2.getString(1);		
+		//		}
+		//	} finally {
+		//		rs2.close
+		//	}
+		//	st.close();
+		//	c.close();
+		//}
 
 		for(int i = 0;i<questions.length;i++){
 			if(questions[i].type == "Multiple Choice"){
